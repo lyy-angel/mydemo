e62d44ff1bda6823f672f37475898ed2cfac11b0
code1031 <wangzijie1031@qq.com>
2017-04-01 10:57:41 +0800

    调整登录信息的代码位置

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($){
+define(['jquery','template','cookie'],function($,template){
     // 控制左侧导航菜单的折叠
     $('.navs ul').prev('a').on('click', function () {
         $(this).next().slideToggle();
@@ -8,5 +8,25 @@ define(['jquery','cookie'],function($){
     var pathname = location.pathname;
     if(!$.cookie('PHPSESSID') && pathname != '/login' && pathname != '/'){
         location.href = '/login';
+    }
+
+    // 初始化登录成功信息
+    var info = $.cookie('loginInfo');
+    if(info){
+        // 模板内容
+        var tpl = '<div class="avatar img-circle">'+
+                    '<img src="{{tc_avatar}}">'+
+                  '</div>'+
+                  '<h4>{{tc_name}}</h4>';
+        // 数据
+        info = JSON.parse(info);
+        // 模板+数据->静态html片段
+        var render = template.compile(tpl);
+        var html = render(info);
+        // 把静态html片段追加到页面
+        $('.aside .profile').html(html);
+
+        // $('.aside .profile img').attr('src',info.tc_avatar);
+        // $('.aside .profile h4').html(info.tc_name);
     }
 });
